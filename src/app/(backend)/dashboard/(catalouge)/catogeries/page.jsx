b050fa2c47204b4e597803298587919f0734b502
Abdullah Ansari <137299911+Ansari839@users.noreply.github.com>
2024-09-24 14:21:59 +0000

    Form Working Start

--- src/app/(backend)/dashboard/(catalouge)/catogeries/page.jsx
+++ src/app/(backend)/dashboard/(catalouge)/catogeries/page.jsx
@@ -1,10 +1,20 @@
-
+import Heading from "@/app/components/backend/Heading";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 
 export default function page() {
-    return (
-      <div className="flex items-center justify-center flex-col min-h-screen">
-        <h1 className="text-4xl">Categories</h1>
+  return (
+    <div>
+      <div className="flex justify-between ">
+        <Heading title="Categories" />
+        <Link
+          className="flex items-center space-x-3 "
+          href="/dashboard/catogeries/new"
+        >
+          <Plus />
+          <span>Add Category</span>
+        </Link>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
---
+++ src/app/(backend)/dashboard/(catalouge)/catogeries/update/[id]/page.jsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="flex items-center justify-center flex-col min-h-screen">
+      <h1 className="text-4xl">Update Categories</h1>
+    </div>
+  );
+}
